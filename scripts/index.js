3281cf1f102619cf5f68572e7d46d5eee0495c11
Liza Vasina <liz.vasina@gmail.com>
2020-10-10 09:58:54 +0300

    feature: add photos-popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,14 @@
 //кнопки
 let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button');
+let addButton = document.querySelector('.profile__add-button');
+let infoCloseButton = document.querySelector('.popup__close-button_place_info');
+let photosCloseButton = document.querySelector('.popup__close-button_place_photos');
 
 // попап и форма в нем
-let popup = document.querySelector('.popup');
-let form = document.querySelector('.popup__form');
+let popupInfo = document.querySelector('.popup_type_info');
+let popupPhotos = document.querySelector('.popup_type_photos');
+let formInfo = document.querySelector('.popup__form_type_info');
+let formPhotos = document.querySelector('.popup__form_type_photos');
 
 // переменные имени профиля
 let name = document.querySelector('.profile__name');
@@ -15,15 +19,17 @@ let description = document.querySelector('.profile__description');
 let inputDescription = document.querySelector('.popup__text_type_description');
 
 // функция открытия попап окна
-function openPopup() {
-  popup.classList.add('popup_opened');
-  inputName.value = name.textContent;
-  inputDescription.value = description.textContent;
+function openPopup(popupName) {
+  popupName.classList.add('popup_opened');
+  if (popupName == popupInfo) {
+    inputName.value = name.textContent;
+    inputDescription.value = description.textContent;
+  }
 }
 
 // функция закрытия попап окна
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function closePopup(popupName) {
+  popupName.classList.remove('popup_opened');
 }
 
 // обработчик формы
@@ -32,9 +38,11 @@ function formSubmitHandler (evt) {
 
   name.textContent = inputName.value;
   description.textContent = inputDescription.value;
-  closePopup();
+  closePopup(popupInfo);
 }
 
-editButton.addEventListener('click', openPopup);
-form.addEventListener('submit', formSubmitHandler);
-closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', () => openPopup(popupInfo));
+formInfo.addEventListener('submit', formSubmitHandler);
+infoCloseButton.addEventListener('click', () => closePopup(popupInfo));
+photosCloseButton.addEventListener('click', () => closePopup(popupPhotos));
+addButton.addEventListener('click', () => openPopup(popupPhotos));
