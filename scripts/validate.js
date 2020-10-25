a073a66ab04df6631760fb4b7b813d1440dab300
Liza Vasina <liz.vasina@gmail.com>
2020-10-25 23:34:16 +0300

    feature: add object to enable validation func

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,54 +1,54 @@
 
 // показываем сообщение об ошибке
-function showError(input) {
+function showError(input, inputErrorClass) {
   const error = document.querySelector(`#${input.id}-error`);
-  input.classList.add('popup__text_state_invalid');
+  input.classList.add(inputErrorClass);
   error.textContent = input.validationMessage;
 }
 
 // скрываем сообщение об ошибке
-function hideError(input) {
+function hideError(input, inputErrorClass) {
   const error = document.querySelector(`#${input.id}-error`);
-  input.classList.remove('popup__text_state_invalid');
+  input.classList.remove(inputErrorClass);
   error.textContent = '';
 }
 
 // смена активности кнопки
-function toggleButtonState(buttonElement, isActive) {
+function toggleButtonState(buttonElement, inactiveButtonClass, isActive) {
   if (isActive) {
     buttonElement.disabled = false;
-    buttonElement.classList.remove('popup__button_invalid');
+    buttonElement.classList.remove(inactiveButtonClass);
   } else {
     buttonElement.disabled = true;
-    buttonElement.classList.add('popup__button_invalid');
+    buttonElement.classList.add(inactiveButtonClass);
   }
 }
 
 // проверка полей
-function checkInputValidity(input) {
+function checkInputValidity(input, inputErrorClass) {
   if (!input.validity.valid) {
-    showError(input);
+    showError(input, inputErrorClass);
   } else {
-    hideError(input);
+    hideError(input, inputErrorClass);
   }
 }
 
 // присваивание слушателей событий
-function setEventListeners(formElement, buttonElement) {
-  const inputList = Array.from(formElement.querySelectorAll('.popup__text'));
+function setEventListeners(formElement, buttonElement, inputSelector, inactiveButtonClass ,inputErrorClass) {
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
 
   inputList.forEach((input) => {
     input.addEventListener('input', (evt) => {
-      checkInputValidity(evt.target);
+      checkInputValidity(evt.target, inputErrorClass);
 
       const isValid = formElement.checkValidity();
-      toggleButtonState(buttonElement, isValid);
+      toggleButtonState(buttonElement, inactiveButtonClass, isValid);
     });
   });
 }
 
-function enableValidation({formSelector, buttonSelector}) {
-  const formList = Array.from(document.querySelectorAll('.popup__form'));
+function enableValidation({formSelector, inputSelector, buttonSelector, inactiveButtonClass, inputErrorClass}) {
+  const formList = Array.from(document.querySelectorAll(formSelector));
 
   formList.forEach((form) => {
     form.addEventListener('submit', (evt) => {
@@ -57,14 +57,18 @@ function enableValidation({formSelector, buttonSelector}) {
 
     const buttonElement = form.querySelector(buttonSelector);
 
-    setEventListeners(form, buttonElement);
-    toggleButtonState(buttonElement, form.checkValidity());
+    setEventListeners(form, buttonElement, inputSelector, inactiveButtonClass ,inputErrorClass);
+    toggleButtonState(buttonElement, inactiveButtonClass, form.checkValidity());
   });
 }
 
+
 enableValidation({
   formSelector: '.popup__form',
-  buttonSelector: '.popup__button'
+  inputSelector: '.popup__text',
+  buttonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_invalid',
+  inputErrorClass: 'popup__text_state_invalid',
 });
 
 // изначально делаем кнопку сохранить в попапе редактирования профиля активной
