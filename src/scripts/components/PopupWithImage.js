4f85024e0980c0e24de6acc9b48e5fd8bac209a4
Liza Vasina <liz.vasina@gmail.com>
2020-12-01 11:37:00 +0300

    fix: change variables in popup with image class

--- src/scripts/components/PopupWithImage.js
+++ src/scripts/components/PopupWithImage.js
@@ -3,11 +3,13 @@ import { Popup } from './Popup.js';
 export class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._picture = this._popup.querySelector('.pic-popup__image');
+    this._caption = this._popup.querySelector('.pic-popup__caption');
   }
   open(name, link) {
     super.open();
-    this._popup.querySelector('.pic-popup__image').src = link;
-    this._popup.querySelector('.pic-popup__image').alt = name;
-    this._popup.querySelector('.pic-popup__caption').textContent = name;
+    this._picture.src = link;
+    this._picture.alt = name;
+    this._caption.textContent = name;
   }
 }
