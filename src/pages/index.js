80f9af365f39106fd856989504a8e034c6579ca4
Liza Vasina <liz.vasina@gmail.com>
2020-12-08 00:00:49 +0300

    feature: nearly finish avatar updating

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,15 +13,18 @@ import { initialCards,
         formConfig,
         formInfo,
         formPhotos,
+        formAvatar,
+        profileAvatar,
         gridContainer,
         cardTemplate,
         editButton,
         addButton,
+        editAvatarButton,
         inputPicName,
         inputLink } from '../scripts/utils/constants.js';
 
 const userInfo = new UserInfo('.profile__name', '.profile__description');
-const profileAvatar = document.querySelector('.profile__avatar');
+
 
 const api = new Api({
   url: 'https://mesto.nomoreparties.co/v1/cohort-18/',
@@ -71,6 +74,9 @@ infoFormValidation.enableValidation();
 
 const photosFormValidation = new FormValidation(formConfig, formPhotos);
 photosFormValidation.enableValidation();
+
+const avatarFormValidation = new FormValidation(formConfig, formAvatar);
+avatarFormValidation.enableValidation();
 
 //функция создания карточки
 function createCard(name, link, templateSelector, handleCardClick) {
@@ -143,8 +149,22 @@ const popupWithSubmit = new PopupWithSubmit({
 });
 popupWithSubmit.setEventListeners();
 
+const popupAvatarClass = new PopupWithForm({
+  popupSelector: '.popup_type_edit-avatar',
+  handleFormSubmit: (value) => {
+    console.log(value);
+    popupAvatarClass.close();
+  }
+});
+popupAvatarClass.setEventListeners();
 
 // обработчики
+// кнопка редактирования аватара
+editAvatarButton.addEventListener('click', () => {
+  popupAvatarClass.open();
+  avatarFormValidation.enableValidation();
+})
+
 // кнопка редактирования профиля
 editButton.addEventListener('click', () => {
   popupInfoClass.open();
--- src/scripts/utils/constants.js
+++ src/scripts/utils/constants.js
@@ -38,8 +38,12 @@ export const formConfig = {
 // попапы профиля и добавления фото и формы в них
 export const popupInfo = document.querySelector('.popup_type_info');
 export const popupPhotos = document.querySelector('.popup_type_photos');
+export const popupAvatar = document.querySelector('.popup_type_edit-avatar');
 export const formInfo = popupInfo.querySelector('.popup__form_type_info');
 export const formPhotos = popupPhotos.querySelector('.popup__form_type_photos');
+export const formAvatar = popupAvatar.querySelector('.popup__form_type_edit-avatar');
+
+export const profileAvatar = document.querySelector('.profile__avatar');
 
 // фото галерея
 export const gridContainer = document.querySelector('.photo-grid');
@@ -50,6 +54,7 @@ export const cardTemplate = document.querySelector('#card-template');
 //кнопки
 export const editButton = document.querySelector('.profile__edit-button');
 export const addButton = document.querySelector('.profile__add-button');
+export const editAvatarButton = document.querySelector('.profile__edit-avatar-button');
 
 // переменные формы добавления карточки
 export const inputPicName = popupPhotos.querySelector('.popup__text_type_pic-name');
